e58c235e9608ab4d5480fcf6436e705463b834d2
jacky2252v <vivekbaraiya000@gmail.com>
2025-01-10 16:23:22 +0530

    13 languages added

--- src/app/LanguageSwitcher.js
+++ src/app/LanguageSwitcher.js
@@ -12,9 +12,22 @@ const LanguageSwitcher = () => {
   };
 
   return (
-    <div className='text-center'>
-      <button className='text-2xl m-1 p-2 bg-slate-600 text-black hover:bg-black hover:text-white transition-all duration-300 rounded' onClick={() => handleLanguageChange('en')}>English</button>
-      <button className='text-2xl m-1 p-2 bg-slate-600 text-black hover:bg-black hover:text-white transition-all duration-300 rounded' onClick={() => handleLanguageChange('es')}>Español</button>
+    <div className='text-center grid grid-cols-3'>
+      <button className='text-2xl m-3 p-2 bg-slate-600 text-black hover:bg-black hover:text-white transition-all duration-300 rounded' onClick={() => handleLanguageChange('en')}>English</button>
+      <button className='text-2xl m-3 p-2 bg-slate-600 text-black hover:bg-black hover:text-white transition-all duration-300 rounded' onClick={() => handleLanguageChange('hi')}>Hindi</button>
+      <button className='text-2xl m-3 p-2 bg-slate-600 text-black hover:bg-black hover:text-white transition-all duration-300 rounded' onClick={() => handleLanguageChange('ar')}>Arabic</button>
+      <button className='text-2xl m-3 p-2 bg-slate-600 text-black hover:bg-black hover:text-white transition-all duration-300 rounded' onClick={() => handleLanguageChange('es')}>Spanish</button>
+      <button className='text-2xl m-3 p-2 bg-slate-600 text-black hover:bg-black hover:text-white transition-all duration-300 rounded' onClick={() => handleLanguageChange('de')}>German</button>
+      <button className='text-2xl m-3 p-2 bg-slate-600 text-black hover:bg-black hover:text-white transition-all duration-300 rounded' onClick={() => handleLanguageChange('zh')}>Mandarin Chinese</button>
+      <button className='text-2xl m-3 p-2 bg-slate-600 text-black hover:bg-black hover:text-white transition-all duration-300 rounded' onClick={() => handleLanguageChange('ru')}>Russian</button>
+      <button className='text-2xl m-3 p-2 bg-slate-600 text-black hover:bg-black hover:text-white transition-all duration-300 rounded' onClick={() => handleLanguageChange('it')}>Italian</button>
+      <button className='text-2xl m-3 p-2 bg-slate-600 text-black hover:bg-black hover:text-white transition-all duration-300 rounded' onClick={() => handleLanguageChange('fr')}>French</button>
+      <button className='text-2xl m-3 p-2 bg-slate-600 text-black hover:bg-black hover:text-white transition-all duration-300 rounded' onClick={() => handleLanguageChange('pt')}>Portuguese</button>
+      <button className='text-2xl m-3 p-2 bg-slate-600 text-black hover:bg-black hover:text-white transition-all duration-300 rounded' onClick={() => handleLanguageChange('mr')}>Marathi</button>
+      <button className='text-2xl m-3 p-2 bg-slate-600 text-black hover:bg-black hover:text-white transition-all duration-300 rounded' onClick={() => handleLanguageChange('bn')}>Bengali</button>
+      <button className='text-2xl m-3 p-2 bg-slate-600 text-black hover:bg-black hover:text-white transition-all duration-300 rounded' onClick={() => handleLanguageChange('ne')}>Nepali</button>
+      <button className='text-2xl m-3 p-2 bg-slate-600 text-black hover:bg-black hover:text-white transition-all duration-300 rounded' onClick={() => handleLanguageChange('kn')}>Kannada</button>
+      <button className='text-2xl m-3 p-2 bg-slate-600 text-black hover:bg-black hover:text-white transition-all duration-300 rounded' onClick={() => handleLanguageChange('ta')}>Tamil</button>
     </div>
   );
 };
--- src/i18n.js
+++ src/i18n.js
@@ -2,14 +2,40 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import enTranslation from './locales/en.json';
+import hiTranslation from './locales/hi.json';
+import arTranslation from './locales/ar.json';
 import esTranslation from './locales/es.json';
+import deTranslation from './locales/de.json';
+import zhTranslation from './locales/zh.json';
+import ruTranslation from './locales/ru.json';
+import itTranslation from './locales/it.json';
+import frTranslation from './locales/fr.json';
+import ptTranslation from './locales/pt.json';
+import mrTranslation from './locales/mr.json';
+import bnTranslation from './locales/bn.json';
+import neTranslation from './locales/ne.json';
+import knTranslation from './locales/kn.json';
+import taTranslation from './locales/ta.json';
 
 i18n
   .use(initReactI18next) // Initialize i18next for React
   .init({
     resources: {
-      en: { translation: enTranslation }, // English translations
-      es: { translation: esTranslation }, // Spanish translations
+      en: { translation: enTranslation },
+      hi: { translation: hiTranslation },
+      ar: { translation: arTranslation },
+      es: { translation: esTranslation },
+      de: { translation: deTranslation },
+      zh: { translation: zhTranslation },
+      ru: { translation: ruTranslation },
+      it: { translation: itTranslation },
+      fr: { translation: frTranslation },
+      pt: { translation: ptTranslation },
+      mr: { translation: mrTranslation },
+      bn: { translation: bnTranslation },
+      ne: { translation: neTranslation },
+      kn: { translation: knTranslation },
+      ta: { translation: taTranslation }
     },
     lng: 'en', // Default language
     fallbackLng: 'en', // Fallback language if translation is missing
